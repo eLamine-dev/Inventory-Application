f527fe35e8ed921347a119f0a21824ae78bb6cd7
e-lamine <lamine@e-lamine.dev>
2024-11-29 21:47:42 +0100

    changed modals using HTML dialog tag

--- public/scripts.js
+++ public/scripts.js
@@ -15,54 +15,55 @@ function selectItem(itemId) {
 
 function openCategoryModal(mode, category = {}) {
    const modal = document.getElementById('categoryModal');
-   const form = document.getElementById('categoryForm');
-   const title = document.getElementById('categoryModalTitle');
+   // const form = modal.getElementById('categoryForm');
+   // const title = document.getElementById('categoryModalTitle');
 
-   form.reset();
-   if (mode === 'edit') {
-      title.textContent = 'Edit Category';
-      form.action = `/categories/${category.id}?_method=PUT`;
-      document.getElementById('categoryName').value = category.name;
-      document.getElementById('categorySlug').value = category.slug;
-   } else {
-      title.textContent = 'Add Category';
-      form.action = '/categories';
-   }
-   modal.style.display = 'block';
+   // form.reset();
+   // if (mode === 'edit') {
+   //    title.textContent = 'Edit Category';
+   //    form.action = `/categories/${category.id}?_method=PUT`;
+   //    document.getElementById('categoryName').value = category.name;
+   //    document.getElementById('categorySlug').value = category.slug;
+   // } else {
+   //    title.textContent = 'Add Category';
+   //    form.action = '/categories';
+   // }
+   modal.showModal();
 }
 
 function openItemModal(mode, item = {}) {
    const modal = document.getElementById('itemModal');
-   const form = document.getElementById('itemForm');
-   const title = document.getElementById('itemModalTitle');
+   // const form = document.getElementById('itemForm');
+   // const title = document.getElementById('itemModalTitle');
 
-   form.reset();
-   if (mode === 'edit') {
-      title.textContent = 'Edit Item';
-      form.action = `/items/${item.id}?_method=PUT`;
-      document.getElementById('itemName').value = item.name;
-      document.getElementById('itemPrice').value = item.price;
-      document.getElementById('itemStock').value = item.stock;
-      document.getElementById('itemCategory').value = item.category_id;
-      document.getElementById('itemManufacturer').value = item.manufacturer_id;
-      document.getElementById('itemSpecifications').value = JSON.stringify(
-         item.specifications
-      );
-   } else {
-      title.textContent = 'Add Item';
-      form.action = '/items';
-   }
-   modal.style.display = 'block';
+   // form.reset();
+   // if (mode === 'edit') {
+   //    title.textContent = 'Edit Item';
+   //    form.action = `/items/${item.id}?_method=PUT`;
+   //    document.getElementById('itemName').value = item.name;
+   //    document.getElementById('itemPrice').value = item.price;
+   //    document.getElementById('itemStock').value = item.stock;
+   //    document.getElementById('itemCategory').value = item.category_id;
+   //    document.getElementById('itemManufacturer').value = item.manufacturer_id;
+   //    document.getElementById('itemSpecifications').value = JSON.stringify(
+   //       item.specifications
+   //    );
+   // } else {
+   //    title.textContent = 'Add Item';
+   //    form.action = '/items';
+   // }
+   modal.showModal();
 }
 
 function openDeleteItemModal(itemId, itemName) {
    const modal = document.getElementById('deleteItemModal');
-   const form = document.getElementById('deleteItemForm');
-   const modalTitle = document.getElementById('deleteItemModalTitle');
+   // const form = document.getElementById('deleteItemForm');
+   // const modalTitle = document.getElementById('deleteItemModalTitle');
 
-   modalTitle.textContent = `Delete Item: ${itemName}`;
-   form.action = `/items/${itemId}`;
-   modal.style.display = 'block';
+   // modalTitle.textContent = `Delete Item: ${itemName}`;
+   // form.action = `/items/${itemId}`;
+   // modal.style.display = 'block';
+   modal.showModal();
 }
 
 function closeModal(modalId) {
